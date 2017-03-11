2e495e0d3d1e004f60f628113dcac497162bad3f
Josh Hathcock <jdspiral@gmail.com>
2017-03-11 14:22:26 -0500

    Minor line adjustment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyCKCBWWSMuelxuAHSh4l7x2oNSyNzq2I78';
@@ -21,7 +20,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
