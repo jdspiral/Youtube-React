754b307e85e5a0ac688c1ce56e398ead88f142bd
Josh Hathcock <jdspiral@gmail.com>
2017-03-11 14:22:57 -0500

    Import video list

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 
 const API_KEY = 'AIzaSyCKCBWWSMuelxuAHSh4l7x2oNSyNzq2I78';
 
