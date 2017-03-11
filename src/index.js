088a81a83d29cf9b409003658ee386a37dab7b91
Josh Hathcock <jdspiral@gmail.com>
2017-03-11 13:39:58 -0500

    Add constructor method to return YT results

--- src/index.js
+++ src/index.js
@@ -6,12 +6,25 @@ import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyCKCBWWSMuelxuAHSh4l7x2oNSyNzq2I78';
 
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
