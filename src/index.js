02b0532d5a976c7757d7f617aa8051a4c6fe0cf0
Josh Hathcock <jdspiral@gmail.com>
2017-03-11 18:37:38 -0500

    Import video detail component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 const API_KEY = 'AIzaSyCKCBWWSMuelxuAHSh4l7x2oNSyNzq2I78';
 
