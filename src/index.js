99ea4acdf83695db35a8a2e00cccc731c211d0c8
Josh Hathcock <jdspiral@gmail.com>
2017-03-11 13:39:31 -0500

    Turn app into a class based component

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyCKCBWWSMuelxuAHSh4l7x2oNSyNzq2I78';
 
-const App = () => {
     return (
         <div>
             <SearchBar />
         </div>
     );
+class App extends Component {
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
