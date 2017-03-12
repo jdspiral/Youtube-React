399c2dae8a595aa8dcdf97998a42e06ec602eb3e
Josh Hathcock <jdspiral@gmail.com>
2017-03-11 19:05:33 -0500

    Revert "Add boilerplate code and youtube API key"
    This reverts commit 2b1d3b41e125befcee4757f2c86e109340bfa6f1.
    
    # Conflicts:
    #	src/index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/SearchBar';
-import VideoList from './components/VideoList';
-import VideoDetail from './components/VideoDetail';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const API_KEY = 'AIzaSyCKCBWWSMuelxuAHSh4l7x2oNSyNzq2I78';
+import App from './components/app';
+import reducers from './reducers';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        };
-
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <SearchBar />
-                <VideoDetail video={this.state.selectedVideo}/>
-                <VideoList
-                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-                    videos={this.state.videos} />
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
